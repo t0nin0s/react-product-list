d7018fee0def51dde752232c28d664b8f92ac716
Toni Sanchez <toninos@gmail.com>
2017-01-13 16:39:15 +0000

    More Styling

--- app/components/header.js
+++ app/components/header.js
@@ -6,35 +6,37 @@ import styles from '../css/header.scss'
 class Header extends Component {
   render(){
     return (
-      <nav className="nav-main">
-        <div className="logo">Website</div>
-        <ul>
-          <li>
-            <IndexLink to="/" className="nav-item " activeClassName='nav-active-item'>
-              Home <span className="dropdown"/>
-            </IndexLink>
-            <ul className="sub-nav">
-              <li><Link to="/#" >this is a submenu 1</Link></li>
-              <li><Link to="/#">this is another submenu</Link></li>
-              <li><Link to="/#">and another</Link></li>
-            </ul>
-          </li>
-          <li><Link to="/products"  className="nav-item" activeClassName='nav-active-item' >Products</Link></li>
-          <li>
-            <Link className="nav-item" activeClassName='nav-active-item'>
-              Dropdown 2 <span className="dropdown"/>
-            </Link>
-            <ul className="sub-nav">
-              <li><Link to="/#" >this is a submenu 2</Link></li>
-              <li><Link to="/#">this is yet another</Link></li>
-              <li><Link to="/#">and another 2</Link></li>
-            </ul>
-          </li>
-          <li><Link to="/about"  className="nav-item" activeClassName='nav-active-item'>About</Link></li>
-          <li><Link to="/contact"  className="nav-item" activeClassName='nav-active-item'>Contact</Link></li>
-
-        </ul>
-      </nav>
+      <div className="header">
+        <a className="logo" href="/">Website</a>
+        <span tabIndex="0" className="burger-icon"></span>
+        <nav className="nav-main">
+          <ul>
+            <li>
+              <IndexLink to="/" className="nav-item " activeClassName='nav-active-item'>
+                Home <span className="dropdown"/>
+              </IndexLink>
+              <ul className="sub-nav">
+                <li><Link to="#" className="nav-item">this is a submenu 1</Link></li>
+                <li><Link to="#" className="nav-item">this is another submenu</Link></li>
+                <li><Link to="#" className="nav-item">and another</Link></li>
+              </ul>
+            </li>
+            <li><Link to="/products" className="nav-item" activeClassName='nav-active-item' >Products</Link></li>
+            <li><Link to="/about" className="nav-item" activeClassName='nav-active-item' >About</Link></li>
+            <li><Link to="/contact" className="nav-item" activeClassName='nav-active-item' >Contact</Link></li>
+            <li>
+              <Link className="nav-item" activeClassName='nav-active-item'>
+                Dropdown 2 <span className="dropdown"/>
+              </Link>
+              <ul className="sub-nav">
+                <li><Link to="#" className="nav-item">this is a submenu 2</Link></li>
+                <li><Link to="#" className="nav-item">this is yet another</Link></li>
+                <li><Link to="#" className="nav-item">and another 2</Link></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
     )
   }
 }
